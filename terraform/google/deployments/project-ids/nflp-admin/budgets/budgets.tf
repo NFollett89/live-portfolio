1542b3fa8d8a012ddf4eab189942c3d4987de620
Nick Follett <nfollett89@gmail.com>
2024-07-10 15:19:39 -0400

    Expand the budget period to end of 2025, drop the forecast since it's no longer monthly.

--- terraform/google/deployments/project-ids/nflp-admin/budgets/budgets.tf
+++ terraform/google/deployments/project-ids/nflp-admin/budgets/budgets.tf
@@ -16,16 +16,23 @@ resource "google_billing_budget" "trial" {
     }
   }
 
-  # Additional rule for forecasted spend at 100%
-  threshold_rules {
-    threshold_percent = 1.0
-    spend_basis       = "FORECASTED_SPEND"
-  }
-
   all_updates_rule {
     pubsub_topic   = data.terraform_remote_state.pubsub.outputs.budget_topic_id
     schema_version = "1.0"
   }
 
-  # The absence of budget_filter block means the budget applies to all services in all projects
+  budget_filter {
+    custom_period {
+      start_date {
+        year  = 2024
+        month = 1
+        day   = 1
+      }
+      end_date {
+        year  = 2025
+        month = 12
+        day   = 31
+      }
+    }
+  }
 }
--- terraform/google/deployments/project-ids/nflp-admin/budgets/locals.tf
+++ terraform/google/deployments/project-ids/nflp-admin/budgets/locals.tf
@@ -1,4 +1,6 @@
 locals {
-  project    = "nflp-admin"
+  project = "nflp-admin"
+
+  # I expect to spend slowly, so would like gradual warnings
   thresholds = [0.25, 0.50, 0.75, 0.95, 1.0]
 }
